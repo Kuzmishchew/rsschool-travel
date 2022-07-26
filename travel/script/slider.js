e16006aac98e995e0382517766da2a67a34c763a
Kuzmishchew Max <kuzmishchew@gmail.com>
2022-07-26 11:45:41 +0500

    fix: change style for slider-wrapper for adaptive

---
+++ travel/script/slider.js
@@ -0,0 +1,8 @@
+const wrapper = document.getElementsByClassName("slides-wrapper")[0];
+const slides = document.getElementsByClassName("slides-item");
+
+for (let slide of slides) {
+    slide.addEventListener("click", () => {
+        
+    })
+}
