23e89fdbc62f9bb3f210ef6f5f48d7fd6ede0dd2
Kuzmishchew Max <kuzmishchew@gmail.com>
2022-07-26 23:12:10 +0500

    feat: add slider activity for desktop

--- travel/script/slider.js
+++ travel/script/slider.js
@@ -1,8 +1,66 @@
 const wrapper = document.getElementsByClassName("slides-wrapper")[0];
 const slides = document.getElementsByClassName("slides-item");
+const radio = document.getElementsByClassName("slide-checker");
+const firstSlide = slides[0];
+const secondSlide = slides[1];
+const thirdSlide = slides[2];
 
-for (let slide of slides) {
-    slide.addEventListener("click", () => {
-        
-    })
-}
+
+radio[1].style.backgroundColor = "rgba(242, 120, 92)";
+
+firstSlide.addEventListener("click", () => {
+    let margin = window.getComputedStyle(secondSlide, null).getPropertyValue("margin-right");
+    let dist = +thirdSlide.offsetWidth + parseInt(margin, 10);
+    firstSlide.style.left = `${dist}px`;
+    secondSlide.style.left = `${dist}px`;
+    thirdSlide.style.left = `${dist}px`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+});
+radio[0].addEventListener("click", () => {
+    let margin = window.getComputedStyle(secondSlide, null).getPropertyValue("margin-right");
+    let dist = +thirdSlide.offsetWidth + parseInt(margin, 10);
+    firstSlide.style.left = `${dist}px`;
+    secondSlide.style.left = `${dist}px`;
+    thirdSlide.style.left = `${dist}px`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+});
+radio[1].addEventListener("click", () => {
+    thirdSlide.style.left = `0`;
+    firstSlide.style.left = `0`;
+    secondSlide.style.left = `0`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+});
+secondSlide.addEventListener("click", () => {
+    thirdSlide.style.left = `0`;
+    firstSlide.style.left = `0`;
+    secondSlide.style.left = `0`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+});
+radio[2].addEventListener("click", () => {
+    let margin = window.getComputedStyle(secondSlide, null).getPropertyValue("margin-right");
+    let dist = +thirdSlide.offsetWidth + parseInt(margin, 10);
+    thirdSlide.style.left = `-${dist}px`;
+    firstSlide.style.left = `-${dist}px`;
+    secondSlide.style.left = `-${dist}px`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92)";
+});
+thirdSlide.addEventListener("click", () => {
+    let margin = window.getComputedStyle(secondSlide, null).getPropertyValue("margin-right");
+    let dist = +thirdSlide.offsetWidth + parseInt(margin, 10);
+    thirdSlide.style.left = `-${dist}px`;
+    firstSlide.style.left = `-${dist}px`;
+    secondSlide.style.left = `-${dist}px`;
+    radio[0].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[1].style.backgroundColor = "rgba(242, 120, 92, 0.7)";
+    radio[2].style.backgroundColor = "rgba(242, 120, 92)";
+});
